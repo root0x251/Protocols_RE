b8976c713baf4b943a5b8f6a580b2ec9bf120325
Slava <rtootik@gmail.com>
2017-02-21 15:35:11 +0300

    Initial Commit

---
+++ Proptocols/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Proptocols
+//
+//  Created by Slava on 21.02.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Proptocols/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Proptocols
+//
+//  Created by Slava on 21.02.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
